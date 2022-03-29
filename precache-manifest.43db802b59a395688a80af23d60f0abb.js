3cc468efec16b35d77961d3a3d8b9fadde178418
CI <CI@example.com>
2022-03-29 19:21:30 +0000

    deploy

---
+++ precache-manifest.43db802b59a395688a80af23d60f0abb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d85961b550b4efd7858c003f4bb9f5",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "1cf7e70e4f91fd3b9b4f991d99aff42b",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "ac91b366352c481d22cabd807bc11e3d",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "62fc0e1bdfc4b52fc492",
+    "url": "/css/app.48793355.css"
+  },
+  {
+    "revision": "81165ee1d71fd57062bfdefb729ecfce",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "453098ba2b7c24a1ce4b6aa33de783d3",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "e4556e083bac45d6b8b5d1aadc675a6e",
+    "url": "/img/logo-light.svg"
+  },
+  {
+    "revision": "33f1af83338ad02faab147d1b1d657b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fc0e1bdfc4b52fc492",
+    "url": "/js/app.32282e3c.js"
+  },
+  {
+    "revision": "22ab3eb1d0c6480f906a",
+    "url": "/js/chunk-vendors.544721ae.js"
+  },
+  {
+    "revision": "1147ec455dd62b1a390d8441c9cdae52",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "baa681745f2b0bea04e6d07b0141c990",
+    "url": "/site.webmanifest"
+  }
+]);
